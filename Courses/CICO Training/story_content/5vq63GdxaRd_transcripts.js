6b65dd6846b7fa90bab693ed12227e6683c495b8
SaviSharpe <105891612+SaviSharpe@users.noreply.github.com>
2025-08-25 21:35:16 -0500

    CICO course

---
+++ Courses/CICO Training/story_content/5vq63GdxaRd_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":288,"text":"Once you've checked in to System 1, you'll be able to check in to System 2."},{"start":4032,"text":"From the System 1 check in, you can select Open System 2 to access System 2."},{"start":8909,"text":"Sign in with your unique ID and password."},{"start":11072,"text":"Once signed in, select the Timesheets tab and open today's timesheet."},{"start":15296,"text":"Under the SO you're completing, select Enter Data then Check In Now!"},{"start":19712,"text":"Once the green check confirms your check in, you can begin your scheduled shift."}]}]};
+    window.globalLoadJsAsset('story_content/5vq63GdxaRd_transcripts.js', JSON.stringify(data));
+})();
