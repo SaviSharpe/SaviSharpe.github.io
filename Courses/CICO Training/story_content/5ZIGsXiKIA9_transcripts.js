f8f8c7d7418e2e41aef5e990245c9aade4ccf504
SaviSharpe <105891612+SaviSharpe@users.noreply.github.com>
2025-08-25 21:33:56 -0500

    delete

--- Courses/CICO Training/story_content/5ZIGsXiKIA9_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":522,"text":"When you are ready to end your shift, check out of System 1 by opening the System 1 app again"},{"start":5358,"text":"and selecting \"Check out\" at the bottom of the screen."},{"start":8067,"text":"You'll be prompted to confirm you want to check out. Select \"Yes.\""},{"start":12083,"text":"You will then be given a checkout summary and can close the app."}]}]};
-    window.globalLoadJsAsset('story_content/5ZIGsXiKIA9_transcripts.js', JSON.stringify(data));
-})();
